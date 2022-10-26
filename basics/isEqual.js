2aa9f1ae44ba1c9c60155b7c6f8fec5920bf9bbb
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2022-10-25 21:36:07 -0300

    primeiro desafio

---
+++ basics/isEqual.js
@@ -0,0 +1,9 @@
+/* 
+    Escreva uma função que recebe dois valores como parâmetro e retorna se os valor são do mesmo valor e tipo
+*/
+
+function isEqual(a, b) {
+    return a === b;
+};
+
+console.log(isEqual(1, '1'));
