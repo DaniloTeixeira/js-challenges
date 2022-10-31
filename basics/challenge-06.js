678af144b2707a26fb6d47684d0794fd0850e88e
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2022-10-31 18:23:59 -0300

    Ajuste de código

---
+++ basics/challenge-06.js
@@ -0,0 +1,8 @@
+/* 
+ Crie uma função que recebe suas strings como parâmetro (a, b), e retorna o número de vezes que a string "a" aparece dentro de "b"
+*/
+
+function findPharameterAInB(a, b) {
+    return String(b).substring;
+}
+
